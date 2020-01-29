f44ad41639c900d6df46421a2336c27c32ee4505
Quinn1876 <qhodges0442@gmail.com>
2020-01-29 12:10:23 -0500

    Made cross-platform

--- test_runner.py
+++ test_runner.py
@@ -1,28 +1,48 @@
-import sys
 import subprocess
-import os
+import platform
+import sys
+
+NLC = '\\n'
+
+
+def getTestFilesAsList(folder):
+    if platform.system() == 'Windows':
+        ls = subprocess.run(['dir', folder], stdout=subprocess.PIPE)
+    else:
+        ls = subprocess.run(['ls', folder], stdout=subprocess.PIPE)
+    files = str(ls.stdout).lstrip("b'").rstrip("'").replace('\\t', ' ').replace('\\n', ' ').split()
+    outFiles = filter(lambda file: file[-4:] == '.out', files)
+    inFiles = filter(lambda file: file[-3:] == '.in', files)
+    testFiles = {'inFiles': [], 'outFiles': []}
+    for file in inFiles:
+        testFiles['inFiles'].append(folder + '/' + file)
+    for file in outFiles:
+        testFiles['outFiles'].append(folder + '/' + file)
+
+    return testFiles
 
 program = sys.argv[1]
-folder = sys.argv[2]
+testFolder = sys.argv[2]
 
-in_files = []
-with subprocess.Popen(["ls", folder],stdout=subprocess.PIPE) as ls:
-    with subprocess.Popen(["grep", ".in"], stdin=ls.stdout, stdout=subprocess.PIPE) as files:
-        in_files = sorted(files.communicate()[0].splitlines())
+testFiles = getTestFilesAsList(testFolder)
 
-out_files = []
-with subprocess.Popen(["ls", folder],stdout=subprocess.PIPE) as ls:
-    with subprocess.Popen(["grep", ".out"], stdin=ls.stdout, stdout=subprocess.PIPE) as files:
-        out_files = sorted(files.communicate()[0].splitlines())
+for testNumber in range(len(testFiles['inFiles'])):
+    with open(testFiles['inFiles'][testNumber], 'r') as inFile:
+        completedProcess = subprocess.run([program], stdin=inFile, stdout=subprocess.PIPE)
 
-for i, v in enumerate(in_files):
-    with open(folder+"/"+str(v, 'utf-8'), 'r') as f:
-        with open("out", "w+") as outfile:
-            print("-------------------------------------")
-            print("testing file: ", str(v, 'utf-8'))
-            print("-------------------------------------")
-            with subprocess.Popen(program, stdin=f, stdout=outfile) as a:
-                pass
-            with subprocess.Popen(["diff", "out", folder+"/"+str(out_files[i], 'utf-8')], stdout=subprocess.PIPE) as test:
-                print("difference: ") 
-                print(str(test.communicate()[0], 'utf-8'), end='')
+    output = str(completedProcess.stdout).lstrip("b'").rstrip("'")
+    # print(output)
+    output = output.split(NLC)
+
+    with open(testFiles['outFiles'][testNumber], 'r') as outFile:
+        for line in output:
+            expectedOutput = outFile.readline().strip()
+            if expectedOutput != line:
+                print('Expected: ', expectedOutput)
+                print('Recieved: ', line)
+                print('Are equal: ', bool(expectedOutput == line))
+                print('Exiting...')
+    print(f'test {testNumber} passed')
+print('All tests passed')
+
+
